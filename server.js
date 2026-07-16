795c6d4bfaf4c94a6b26bb9c1b6f3b9c45540dd0
bhuvan-codes <bhuvan7115@gmail.com>
2026-07-16 16:36:41 +0530

    add new route

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 app.get('/' , (req , res)=>{
     res.status(200).send('server is running');
 })
+app.get('/hello' , (req , res)=>{
+    res.status(200).json('hello from the EC2 instance');
+})
 app.listen(PORT , ()=>{
     console.log(`server is running on PORT : ${PORT}`);
 })
